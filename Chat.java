5c68532ce5a7650da9b2f252dbbe3858cededbf0
aarishsidhu <43157882+aarishsidhu@users.noreply.github.com>
2019-10-21 22:15:16 -0700

    Add files via upload

---
+++ Chat.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+// server class
+public class Chat {
+    private int port;
+    private Set<String> userNames = new HashSet<>();
+    private Set<UserThread> userThreads = new HashSet<>();
+    private static int uniqueId;
+
+    public Chat(int port) {
+        this.port = port;
+    }
+
+    public void execute(ChatClient client) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            System.out.println("Chat Server is listening on port " + port);
+
+            // client stuff
+            Socket csocket = new Socket(client.getHostname(), client.getPort());
+
+            System.out.println("Connected to the chat server!");
+
+            new ReadThread(csocket, client).start();
+            new WriteThread(csocket, client).start();
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New user connected");
+
+                UserThread newUser = new UserThread(socket, this);
+                userThreads.add(newUser);
+                newUser.start();
+
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error in the server: " + ex.getMessage());
+        }
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
+        if (args.length < 1) {
+            System.out.println("Syntax: java Chat <port-number>");
+            System.exit(0);
+        }
+
+        InetAddress web = InetAddress.getLocalHost();
+        String hostName = web.getHostAddress().toString();
+        int port = Integer.parseInt(args[0]);
+
+        Chat server = new Chat(port);
+        ChatClient client = new ChatClient(hostName, port);
+        server.execute(client);
+    }
+
+    /**
+     * Delivers a message from one user to others (broadcasting)
+     */
+    void broadcast(String message, UserThread excludeUser) {
+        for (UserThread aUser : userThreads) {
+            if (aUser != excludeUser) {
+                aUser.sendMessage(message);
+            }
+        }
+    }
+
+    /**
+     * Stores username of the newly connected client.
+     */
+    void addUserName(String userName) {
+        userNames.add(userName);
+    }
+
+    /**
+     * When a client is disconneted, removes the associated username and UserThread
+     */
+    void removeUser(String userName, UserThread aUser) {
+        boolean removed = userNames.remove(userName);
+        if (removed) {
+            userThreads.remove(aUser);
+            System.out.println("The user " + userName + " quitted");
+        }
+    }
+
+    Set<String> getUserNames() {
+        return this.userNames;
+    }
+
+    /**
+     * Returns true if there are other users connected (not count the currently connected user)
+     */
+    boolean hasUsers() {
+        return !this.userNames.isEmpty();
+    }
+
+
+
+
+
+
+
+
+   /* public void execute() {
+        try {
+            Socket socket = new Socket(hostname, port);
+
+            System.out.println("Connected to the chat server");
+
+            new ReadThread(socket, this).start();
+            new WriteThread(socket, this).start();
+
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+    }*/
+}
+
